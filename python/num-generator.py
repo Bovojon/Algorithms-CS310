e0bc4fa50e6ce7c43c700caee184135b791774b7
Jon <bovojon@gmail.com>
2016-10-21 10:13:16 -0400

    Project 1 - implement random number generator

---
+++ python/num-generator.py
@@ -0,0 +1,17 @@
+import random
+
+def number_generator(n):
+	listNums = []
+	for i in range(n):
+		a = random.randint(0,1000)
+		listNums.append(a)
+
+	f = open('{}.txt'.format(n), 'w')
+	f.write(str(listNums)[1:-1])
+	f.close()
+
+	print(str(listNums)[1:-1])
+
+
+
+number_generator(10);
