09439910ca4100c1cc93a88a760e15aa678e6dd2
Jon <bovojon@gmail.com>
2016-10-23 22:06:30 -0400

    Need to fix errors with selection sort for c

--- sorting/selection-sort.c
+++ sorting/selection-sort.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 
-int main() {
-	int array[100], n, c, d, position, swap;
+int selectionSort(numlist){
+	int c, d, position, swap;
 
-	printf("Enter the number of elements\n");
-	scanf("%d", &n);
+	// printf("Enter the number of elements\n");
+	// scanf("%d", &n);
+	// printf("Enter %d integers\n", n);
 
-	printf("Enter %d integers\n", n);
+	int n = sizeof(numlist);
 
-	for (c=0; c<n; c++){
-		scanf("%d", &array[c]);
-	}
+	// for (c=0; c<n; c++){
+	// 	scanf("%d", &array[c]);
+	// }
 
 	for (c=0; c<(n-1); c++) {
 		position=c;
-		for (d=c+1; d<n; d++){
-			if (array[position] > array[d]){
+		for(d=c+1; d<n; d++){
+			if (numlist[position] > numlist[d]){
 				position = d;
 			}
 		}
 		if(position != c){
-			swap = array[c];
-			array[c] = array[position];
-			array[position] = swap;		
+			swap = numlist[c];
+			numlist[c] = numlist[position];
+			numlist[position] = swap;		
 		}
 	}
 	printf("Sorted List in ascending order:\n");
 	for(c=0; c<n; c++){
-		printf("%d\n", array[c]);
+		printf("%d\n", numlist[c]);
 	}
 
 	return 0;
+}
+
+
+int main(){
+	int numlist[8] = {3,7,2,5,8,1,4,6}; 
+	return selectionSort(numlist);
 }
