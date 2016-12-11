50fee14ff0a62f09920f67ce52198676bfc990ec
Jon <bovojon@gmail.com>
2016-12-10 19:25:38 -0500

    Complete project 2 - building graph with adjacency list implementation and finding strongly connected components

--- python-searching-graphs/graph-classes.py
+++ python-searching-graphs/graph-classes.py
@@ -1,116 +1,180 @@
-class Node:
-    __slots__ = '_city', '_index', '_adjacentNodes'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-    def __init__(self, index, city):
-        self._city = city
-        self._index = index
-        self._adjacentNodes = {}             #  Create dictionary called adjacentNodes
-
-    # Selector functions
-    def getCity(self):
-        return self._city
-
-    def getIndex(self):
-        return self._index
-
-    def getAdjacentNodes(self):
-        return self._adjacentNodes.keys()
-
-    ############################               Fix later
-    def addAdjacentNode(self, node, edgeIndex):
-        self._adjacentNodes[node] = edgeIndex
-    
-    def firstAdjacentNode(self):
-        return self._adjacentNodes[0].keys()
-    ############################
+from glob import glob
 
 
+class Edges():
+    def __init__(self, value):
+        self._value = value
+        self._next = None
 
-class Edge:
-    __slots__ = '_origin', '_destination', '_index'
+    def getName(self):
+        return self._value
 
-    def __init__(self, u, v, index):
-        self._origin = u
-        self._destination = v
-        self._index = index
-
-    def getEndpoints(self):
-        return (self._origin, self._destination)
-
-    def getIndex(self):
-        return self._index
-
-    # Selector function
-    def getOppositeNode(self, v):
-        oppositeNode = self._destination
-        return oppositeNode._index if v is self._origin else self._origin._index
+    def getNext(self):
+        return self._next
 
 
+class Nodes():
+    def __init__(self, value):
+        self._value = value
+        self._pointer = None  # empty
 
-class Graph:
+    def name(self):
+        return self._value
+
+    def setName(self, newname):
+        self._value = newname
+
+    def firstEdge(self):
+        return self._pointer
+
+    def addEdge(self, value):
+        temp = Edges(value)
+        temp._next = self._pointer
+        self._pointer = temp
+
+    def travel(self):
+        edges = []
+        present = self.firstEdge()
+        while present is not None:
+            edges.append(present)
+            present = present.getNext()
+        return edges
+
+
+class Graph():
     def __init__(self):
-        self.numNodes = 0
-        self.nodeList = {}             #  Create dictionary of nodes called nodeList 
+        self.graph = []
 
-    def addNode(self, index, city):
-        self.numNodes = self.numNodes + 1     # Increment number of nodes
-        newNode = Node(index, city)           # Build instance of node class
-        
-#         Need to check if node exists already
-        self.nodeList[index] = newNode
-        return newNode
-        
-        
-    def getNode(self, node):
-        if node in self.nodeList:
-            return self.nodeList[node]
-        else:
-            return None
+    def searchCity(self, value):
+        if value not in self.graph:
+            node = Nodes(value)
+            self.graph.append(node)
+        return self._index(value)
 
-    def addEdge(self, node1, node2, edgeCount):                        # node1 and node2 must already be created with addNode()
-        self.nodeList[node1].addAdjacentNode(self.nodeList[node2], edgeCount)
-
-    def getNodes(self):
-        return self.nodeList.keys()
-
-    def __iter__(self):
-        return iter(self.nodeList.values())
+    def _index(self, value):
+        for n in self.graph:
+            if n.name() == value:
+                return self.graph.index(n)
 
 
+def buildGraph(filename):
+    "Open and read graph from file"""
+    f = open(filename, "r")
+    value = set()
+    edge_adj = []
+    for line in f:
+        line = line.strip('\n')
+        line = line.split(" ")
+        for edge in line:
+            edge = edge.split('-')
+            edge_adj.append(edge)
+            value.add(edge[0])
+            value.add(edge[1])
+            f.close
+
+    # Create instance of Graph class:
+    G = Graph()
+
+    # build the adj-list
+    for i in value:
+        index = G.searchCity(i)
+        for x in edge_adj:
+            if x[0] == i:
+                G.graph[index].addEdge(x[1])
+
+    return G
 
 
-if __name__ == '__main__':
-
-#     Create instance of Graph class:
-    g = Graph()
-    
-#     Begin indexing for edge and node
-    edgeCount = 0
-    nodeCount = 0
-
-    # Open and read from file:
-    file = open("graphSmall1.in")
-    for i in file:
-        i =i.strip('\n')
-        i = i.split(" ")
-        for line in i:
-            nodeCount += 1
-            edgeCount += 1
-            line = line.split('-')
-            node1 = g.addNode(nodeCount, line[0])
-            node2 = g.addNode(nodeCount, line[1])
-            node1Index = node1.getIndex()
-            node2Index = node2.getIndex()
-            g.addEdge(node1Index,node2Index,edgeCount)
-
-#     print(g)
-    for v in g:
-        for w in v.getAdjacentNodes():
-            print("( %s , %s )" % (v.getCity(), w.getCity()))
+def graph_adt_test(G):
+    """test graph abstract data type and print graph nodes"""
+    for i in G.graph:
+        node = i.name()
+        index = G._index(node)
+        print("Node:", index, ",", "Name:", node)
+        edges = G.graph[index].travel()
+        for e in edges:
+            print(e.getName())
+            print("#------------")
 
 
+# --------------------------------------------------------------------------- #
+
+def countEdges(G):
+    """Count the number of edges in the graph data type"""
+    E = []
+    for node in G:
+        for edge in node.travel():
+            E.append(1)
+    return sum(E) // 2
 
 
+def connectedComponents(G):
+    """Find connected components in undirected graph
+    using simplified depth-first-search
+    """
+
+    components = []
+
+    for node in G.graph:
+        stack = []
+        visited = set()
+
+        stack.append(node)
+        visited.add(node.name())
+
+        while stack:
+            connected = stack.pop()
+            for edge in connected.travel():
+                e = edge.getName()
+                if e not in visited:
+                    node = G.graph[G._index(e)]
+                    stack.append(node)
+                    visited.add(e)
+        if visited not in components:
+            components.append(visited)
+
+    return components
 
 
+def outputGraph(G):
+    """Print output for project 2"""
+    cities = len(G.graph)
+    edges = countEdges(G.graph)
+    components = connectedComponents(G)
 
+    print("Read", cities, "cites and", edges, "edges")
+    print("Number of connected components", len(components))
+    count = 0
+    cycle = False
+    for c in components:
+        count += 1
+        C = [G.graph[G._index(x)] for x in c]
+        v = len(c)
+        e = countEdges(C)
+
+        if v <= e:
+            cycle = True
+
+        print("  Connected Component", count)
+        for node in c:
+            print("    ", node)
+
+    if cycle:
+        print("Graph contains a cycle")
+    else:
+        "Graph does not contain a cycle"
+
+# G = build_graph("input/graphSmall3.in")
+# output_graph(G)
+
+if __name__ == "__main__":
+    input_files = glob("input/*")
+
+    for f in input_files:
+        G = buildGraph(f)
+        print(f)
+        outputGraph(G)
+        print()
